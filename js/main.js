36f5f67970a55de7411c497f697ab26011c64beb
Fernando Monares <ticmonares@gmail.com>
2021-06-22 12:06:42 -0500

    Portada backgorund fixed

--- js/main.js
+++ js/main.js
@@ -10,3 +10,45 @@ const listenNavLinks = () => {
 document.addEventListener('DOMContentLoaded', () => {
     listenNavLinks();
 })
+
+/**
+ * * Esta función únicamente asigna la clase show a todos los iconos de líneas tematicas
+ */
+function mostrarIconos() {
+    var iconos = document.querySelectorAll(".img-thumbnail");
+    iconos.forEach(icono => {
+        icono.classList.add("show");
+    });
+}
+
+window.onload = function() {
+    // var header = document.getElementById("sobre-mi");
+    // var sticky = header.offsetTop;
+    window.addEventListener("scroll", function() {
+        let $tematicasFirstRow = document.getElementById("img-porfile");
+        elementIsVisible($tematicasFirstRow);
+    });
+};
+
+/**
+ * * Función para saber cuando un elemento es visible justo cunado
+ * *entra en pantalla, es decir esta visible
+ * * Información importante esta resaltada
+ * ? Sera ideal darle un pequeño rango?
+ */
+const elementIsVisible = ($element) => {
+    //* Obtenemos la altura  del documento cliente
+    let clientHeight = document.documentElement.clientHeight;
+    //* Obtenemos las coordenadas del elemento que entro por parámetro
+    let coords = $element.getBoundingClientRect();
+    // Obtenemos las coordenadas en el eje Y
+    let yCoords = coords.y;
+    // console.log(clientHeight);
+    // console.log(yCoords);
+    // si la altura del cliente es mayor o igual que las coordenadas en y del elemento
+    // Entonces mostramos los iconos
+    if (clientHeight >= yCoords) {
+        console.log("AHORA SE MUESTRA");
+        mostrarIconos()
+    }
+}
